6a33d9b93f5fe5cd852d963a3e8c3f0521249f4d
sirma <sirma@DESKTOP-OG7P7B5>
2023-01-01 21:51:26 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace HeXLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(330, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(126, 125);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.9F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(253, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HeXPLOIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(686, -8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(573, -8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(567, 45);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(394, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(127, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(536, 303);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gold;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(253, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(290, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Download";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(13, 60);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "<--";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeXLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point lastPoint;
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("HeXPLOIT.zip"))
+            {
+                File.Delete("HexPLOIT.zip");
+            }
+
+            if (File.Exists("versioninfo.txt"))
+            {
+                File.Delete("versioninfo.txt");
+            }
+
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit"))
+            {
+
+                button4.Text = "Run";
+            }
+
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit\\versioninfo.txt"))
+            {
+                string contents = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit\\versioninfo.txt");
+                string remoteUri = "https://github.com/HeXedek/HeXPLOIT/releases/download/release/";
+                string fileName = "versioninfo.txt", myStringWebResource = null;
+                WebClient myWebClient = new WebClient();
+                myStringWebResource = remoteUri + fileName;
+                myWebClient.DownloadFile(myStringWebResource, fileName);
+                string contents2 = File.ReadAllText("versioninfo.txt");
+                if(!contents.Equals(contents2))
+                {
+                    button4.Text = "Update";
+                }
+            }
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            panel1.Visible = false;
+            pictureBox1.Visible = true;
+            button4.Visible = true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(button4.Text == "Download")
+            {
+                button4.Text = "Downloading";
+                string remoteUri = "https://github.com/HeXedek/HeXPLOIT/releases/download/release/";
+                string fileName = "HeXPLOIT.zip", myStringWebResource = null;
+                WebClient myWebClient = new WebClient();
+                myStringWebResource = remoteUri + fileName;
+                myWebClient.DownloadFile(myStringWebResource, fileName);
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit");
+                string extractPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit";
+                System.IO.Compression.ZipFile.ExtractToDirectory("HeXPLOIT.zip", extractPath);
+                button4.Text = "Run";
+            }
+
+            if (button4.Text == "Update")
+            {
+                button4.Text = "Downloading";
+                if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings"))
+                {
+                    Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings", true);
+                }    
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings");
+                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit\\settings\\sautoattach.hexploit", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+ "\\hexpsettings\\sautoattach.hexploit");
+                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\settings\\stransparency.hexploit", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings\\stransparency.hexploit");
+                Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit", true);
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\hexploit");
+                string remoteUri = "https://github.com/HeXedek/HeXPLOIT/releases/download/release/";
+                string fileName = "HeXPLOIT.zip", myStringWebResource = null;
+                WebClient myWebClient = new WebClient();
+                myStringWebResource = remoteUri + fileName;
+                myWebClient.DownloadFile(myStringWebResource, fileName);
+                string extractPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit";
+                System.IO.Compression.ZipFile.ExtractToDirectory("HeXPLOIT.zip", extractPath);
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\settings");
+                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings\\sautoattach.hexploit", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\settings\\sautoattach.hexploit");
+                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexpsettings\\stransparency.hexploit", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\settings\\stransparency.hexploit");
+                button4.Text = "Run";
+            }
+
+            if (button4.Text == "Run")
+            {
+                Directory.SetCurrentDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\");
+                Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\hexploit\\HeXPLOIT.exe");
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            panel1.Visible = true;
+            pictureBox1.Visible = false;
+            button4.Visible = false;
+        }
+    }
+}
